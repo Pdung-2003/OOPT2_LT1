1c48fa0368e2d5718c0170285988d37b237b51e5
lephuchung <lephuchung003@gmail.com>
2023-12-01 04:36:47 +0700

    update MyPanel 04h36 1/12

---
+++ src/view/MyPanel.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class MyPanel extends JPanel {
+	
+    public MyPanel() {
+        super();
+        customizeMyPanel();
+    }
+
+    private void customizeMyPanel() {
+        setBackground(Colors.Trang);
+    }
+    
+}
