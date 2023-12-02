df610ca6044e6792debe9c397754b9a2bd731162
lephuchung <lephuchung003@gmail.com>
2023-12-02 22:13:23 +0700

    update ThongKeNFT 22h13 2/12

--- src/view/ThongKeNFT.java
+++ src/view/ThongKeNFT.java
@@ -5,6 +5,12 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JScrollPane;
 
 public class ThongKeNFT extends JPanel {
 
@@ -19,10 +25,61 @@ public class ThongKeNFT extends JPanel {
 		setLayout(new BorderLayout(0, 0));
 		
 		MyPanel panel_TKe_Filter = new MyPanel();
-		add(panel_TKe_Filter, BorderLayout.NORTH);
+		add(panel_TKe_Filter, BorderLayout.WEST);
+		panel_TKe_Filter.setLayout(new BorderLayout(0, 0));
+		
+		MyPanel panel_TKe_Filter_Title = new MyPanel();
+		panel_TKe_Filter.add(panel_TKe_Filter_Title, BorderLayout.WEST);
+		panel_TKe_Filter_Title.setLayout(new GridLayout(10, 1, 10, 30));
+		
+		MyLabel lblNewLabel = new MyLabel("New label");
+		panel_TKe_Filter_Title.add(lblNewLabel);
+		
+		MyLabel lblNewLabel_1 = new MyLabel("New label");
+		panel_TKe_Filter_Title.add(lblNewLabel_1);
+		
+		MyLabel lblNewLabel_2 = new MyLabel("New label");
+		panel_TKe_Filter_Title.add(lblNewLabel_2);
+		
+		MyLabel lblNewLabel_3 = new MyLabel("New label");
+		panel_TKe_Filter_Title.add(lblNewLabel_3);
+		
+		MyPanel panel_TKe_Filter_Option = new MyPanel();
+		panel_TKe_Filter.add(panel_TKe_Filter_Option, BorderLayout.CENTER);
+		panel_TKe_Filter_Option.setLayout(new GridLayout(10, 1, 10, 30));
+		
+		// Chon thong ke ve thong tin 1
+		String[] items_TKe_ThongTin4 = {"Tên NFT", "Chủ bộ sưu tập", "Ngày tạo", "Giá"}; 
+		MyComboBox comboBox = new MyComboBox(items_TKe_ThongTin4);
+		panel_TKe_Filter_Option.add(comboBox);
+		
+		String[] items_TKe_TimTheo = {"Tên NFT", "Chủ bộ sưu tập", "Ngày tạo", "Giá"}; 
+		MyComboBox comboBox_1 = new MyComboBox(items_TKe_TimTheo);
+		panel_TKe_Filter_Option.add(comboBox_1);
+		
+		String[] items_TKe_ThongTin2 = {"Tên NFT", "Chủ bộ sưu tập", "Ngày tạo", "Giá"}; 
+		MyComboBox comboBox_2 = new MyComboBox(items_TKe_ThongTin2);
+		panel_TKe_Filter_Option.add(comboBox_2);
+		
+		String[] items_TKe_ThongTin3 = {"Tên NFT", "Chủ bộ sưu tập", "Ngày tạo", "Giá"}; 
+		MyComboBox comboBox_3 = new MyComboBox(items_TKe_ThongTin3);
+		panel_TKe_Filter_Option.add(comboBox_3);
+		
+		Button_Chung btnNewButton = new Button_Chung("Duyệt");
+		panel_TKe_Filter_Option.add(btnNewButton);
+		
+		MyPanel panel_TKe_Dem = new MyPanel();
+		panel_TKe_Filter.add(panel_TKe_Dem, BorderLayout.EAST);
+		
+		MyPanel panel_TKe_Dem_2 = new MyPanel();
+		panel_TKe_Filter.add(panel_TKe_Dem_2, BorderLayout.NORTH);
 		
 		MyPanel panel_TKe_Content = new MyPanel();
 		add(panel_TKe_Content, BorderLayout.CENTER);
+		panel_TKe_Content.setLayout(new BorderLayout(0, 0));
+		
+		MyScrollPanel scrollPane = new MyScrollPanel();
+		panel_TKe_Content.add(scrollPane, BorderLayout.CENTER);
 		
 	}
 
