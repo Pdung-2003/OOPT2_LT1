66610fdcfae5865188085dfbe39b4d75361edb26
Thanh Nguyen <thnhnguyn761@gmail.com>
2023-12-14 04:01:46 +0700

    Opensea Crawler

---
+++ src/crawler/Opensea/OpenseaCrawler.java
@@ -0,0 +1,155 @@
+package src.crawler.Opensea;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class CollectionInfo {
+    private String collectionName;
+    private String img;
+    private String volume;
+    private String discount;
+    private String floorPrice;
+    private String sales;
+
+    public CollectionInfo(String collectionName, String img, String volume, String discount, String floorPrice, String sales) {
+        this.collectionName = collectionName;
+        this.img = img;
+        this.volume = volume;
+        this.discount = discount;
+        this.floorPrice = floorPrice;
+        this.sales = sales;
+    }
+}
+
+public class OpenseaCrawler {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "chromedriver_win32 (1) - Copy\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensea.io/rankings/trending?sortBy=seven_day_volume");
+
+        try {
+            Thread.sleep(5000);  // Initial wait for the page to load
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Set<String> collectionNames = new HashSet<>();
+        List<CollectionInfo> collectionInfoList = new ArrayList<>();
+        int targetElementCount = 100;
+
+        try {
+            crawlData(driver, collectionNames, collectionInfoList, targetElementCount);
+        } finally {
+            driver.quit();
+        }
+    }
+
+    private static void crawlData(WebDriver driver, Set<String> collectionNames, List<CollectionInfo> collectionInfoList, int targetElementCount) {
+        int currentElementCount = 0;
+
+        while (currentElementCount < targetElementCount) {
+            List<WebElement> currentElements = driver.findElements(By.cssSelector("div.sc-beff130f-0.sc-e7b51c31-0.hksMfk.fWxQZN"));
+
+            for (WebElement element : currentElements) {
+                WebElement collectionNameElement = element.findElement(By.cssSelector("div.sc-beff130f-0.sc-fadcb5cd-3.hksMfk.cqKvtL"));
+                String collectionName = collectionNameElement.getText();
+
+                if (!collectionNames.contains(collectionName)) {
+                    WebElement image = element.findElement(By.tagName("img"));
+                    String img = image.getAttribute("src");
+
+                    WebElement salesElement = element.findElement(By.cssSelector("div.sc-beff130f-0.sc-f89095a4-0.sc-aabd2602-2.cuwMwy.gRSeGa"));
+                    String sales = salesElement.getText();
+
+                    List<WebElement> elementList = element.findElements(By.cssSelector("div.sc-beff130f-0.sc-f89095a4-0.sc-aabd2602-2.hcyTCW.gRSeGa"));
+                    String volume = (elementList.size() >= 1) ? elementList.get(0).getText() : null;
+                    String discount = (elementList.size() >= 2) ? elementList.get(1).getText() : null;
+                    String floorPrice = (elementList.size() >= 3) ? elementList.get(2).getText() : null;
+
+                    if (volume != null && discount != null && floorPrice != null && sales != null) {
+                        collectionInfoList.add(new CollectionInfo(collectionName, img, volume, discount, floorPrice, sales));
+                        collectionNames.add(collectionName);
+                        currentElementCount++;
+                    }
+                }
+            }
+
+            // Check if new elements are loaded
+            int newElementCount = currentElements.size();
+            if (newElementCount == 0) {
+                break;  // No new elements loaded, exit loop
+            }
+
+            // Use keyboard actions to simulate scrolling
+            simulateScrolling(driver);
+
+            // Wait for the content to load
+            waitForLoad(driver);
+
+            if (!infiniteScroll(driver)) {
+                break;
+            }
+        }
+
+        saveDataToJson(collectionInfoList);
+    }
+
+    private static void simulateScrolling(WebDriver driver) {
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.SPACE).build().perform(); // or use .sendKeys(Keys.ARROW_DOWN)
+    }
+
+    private static void waitForLoad(WebDriver driver) {
+        new WebDriverWait(driver, 10).until(webDriver ->
+                ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+    }
+
+    private static boolean infiniteScroll(WebDriver driver) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        long lastHeight = (long) js.executeScript("return document.body.scrollHeight");
+
+        simulateScrolling(driver);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        long newHeight = (long) js.executeScript("return document.body.scrollHeight");
+        return newHeight != lastHeight;
+    }
+
+    private static void saveDataToJson(List<CollectionInfo> collectionInfoList) {
+        JsonArray jsonArray = new JsonArray();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(collectionInfoList);
+        JsonObject mainJsonObject = new JsonObject();
+        mainJsonObject.add("data", jsonArray);
+
+        try (FileWriter fileWriter = new FileWriter("OpenseaCrawlerRankingPer7Days.json")) {
+            fileWriter.write(json);
+            System.out.println("Dữ liệu đã được lưu vào OpenseaCrawlerRankingPer7Days.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/view/ManHinhChinh.java
+++ src/view/ManHinhChinh.java
@@ -1,105 +1,107 @@
-package view;
-
-import OOP.NiftyGateway.NiftygatewayTest;
-import OOP.NiftyGateway.Result;
-
-import java.awt.*;
-import java.io.IOException;
-import java.util.List;
-
-import javax.swing.*;
-
-public class ManHinhChinh extends JFrame {
-
-        /**
-         * Create the panel.
-         */
-        public ManHinhChinh() {
-                setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-                setPreferredSize(new Dimension(1335, 780));
-                setLayout(new BorderLayout(0, 0));
-
-                DanhSachNFT panel_DanhSach = new DanhSachNFT();
-
-                TimKiemNFT panel_TimKiem = new TimKiemNFT();
-
-                ThongKeNFT panel_ThongKe = new ThongKeNFT();
-                // Thực hiện tác vụ chính trong một thread riêng biệt
-                new Thread(() -> {
-                        try {
-                                NiftygatewayTest.main(new String[]{});
-                                BinanceTest.main(new String[]{});
-                                // Truyền các luồng dữ liệu
-                                List<Result> results = NiftygatewayTest.getResults();
-                                List<CollectionItem>  collectionItems = BinanceTest.getCollectionItem();
-
-                                // Truyền dữ liệu vào các panel
-                                panel_DanhSach.setResults(results);
-                                panel_DanhSach.setCollectionItem(collectionItems);
-                                // Gọi các phương thức cần thiết trên các panel khác (nếu cần)
-                        } catch (IOException | InterruptedException e) {
-                                e.printStackTrace();
-                        }
-                }).start();
-
-                SideBar sidebar = new SideBar();
-                add(sidebar, BorderLayout.WEST);
-
-                JPanel content = new JPanel();
-                ;
-                add(content, BorderLayout.CENTER);
-                content.setLayout(new BorderLayout(20, 5));
-
-                JPanel panel_Content_Title = new JPanel();
-                content.add(panel_Content_Title, BorderLayout.NORTH);
-                panel_Content_Title.setLayout(new FlowLayout(FlowLayout.LEFT, 15, 5));
-
-                JLabel lbl_Content_Title = new JLabel("Danh sách NFT"); // ten chuc nang (Danh Sach NFT, Tim Kiem Du Lieu, Thong Ke Du Lieu)
-                lbl_Content_Title.setFont(new Font("Arial", Font.BOLD, 20));
-                panel_Content_Title.add(lbl_Content_Title);
-
-                JPanel panel_Dem_01 = new JPanel();
-                content.add(panel_Dem_01, BorderLayout.WEST);
-
-                JPanel panel_Dem_02 = new JPanel();
-                content.add(panel_Dem_02, BorderLayout.EAST);
-
-                JPanel panel_3 = new JPanel();
-                content.add(panel_3, BorderLayout.SOUTH);
-
-                JPanel panel_Content_Main = new JPanel();
-                content.add(panel_Content_Main, BorderLayout.CENTER);
-                CardLayout cardLayout = new CardLayout(0,0);
-                panel_Content_Main.setLayout(cardLayout);
-                panel_Content_Main.add(panel_DanhSach,"1");
-                panel_Content_Main.add(panel_TimKiem,"2");
-                panel_Content_Main.add(panel_ThongKe,"3");
-
-                Component componentAtIndex = sidebar.getComponent(0);
-                JButton buttonDanhSach = null, buttonTimKiem = null, buttonThongKe = null;
-                if (componentAtIndex instanceof JButton) {
-                        buttonDanhSach = (JButton) componentAtIndex;
-                }
-
-                Component componentAtIndex1 = sidebar.getComponent(1);
-                if (componentAtIndex instanceof JButton) {
-                        buttonTimKiem = (JButton) componentAtIndex1;
-                }
-
-                Component componentAtIndex2 = sidebar.getComponent(2);
-                if (componentAtIndex instanceof JButton) {
-                        buttonThongKe = (JButton) componentAtIndex2;
-                }
-
-                buttonDanhSach.addActionListener(e -> showCard(panel_Content_Main, "1",lbl_Content_Title,"Danh sách NFT"));
-                buttonTimKiem.addActionListener(e -> showCard(panel_Content_Main, "2",lbl_Content_Title,"Tìm kiếm dữ liệu"));
-                buttonThongKe.addActionListener(e -> showCard(panel_Content_Main, "3",lbl_Content_Title,"Thống kê dữ liệu"));
-        }
-
-        private static void showCard(Container container, String cardName, JLabel lbl_Content_Title, String title) {
-                lbl_Content_Title.setText(title);
-                CardLayout cardLayout = (CardLayout) container.getLayout();
-                cardLayout.show(container, cardName);
-        }
-}
+//package src.view;
+//
+//import OOP.NiftyGateway.NiftygatewayTest;
+//import OOP.NiftyGateway.Result;
+//
+//import java.awt.*;
+//import java.io.IOException;
+//import java.util.List;
+//
+//import javax.swing.*;
+//
+//import static sun.jvm.hotspot.code.Location.Type.OOP;
+//
+//public class ManHinhChinh extends JFrame {
+//
+//        /**
+//         * Create the panel.
+//         */
+//        public ManHinhChinh() {
+//                setExtendedState(JFrame.MAXIMIZED_BOTH);
+//
+//                setPreferredSize(new Dimension(1335, 780));
+//                setLayout(new BorderLayout(0, 0));
+//
+//                DanhSachNFT panel_DanhSach = new DanhSachNFT();
+//
+//                TimKiemNFT panel_TimKiem = new TimKiemNFT();
+//
+//                ThongKeNFT panel_ThongKe = new ThongKeNFT();
+//                // Thực hiện tác vụ chính trong một thread riêng biệt
+//                new Thread(() -> {
+//                        try {
+//                                NiftygatewayTest.main(new String[]{});
+//                                BinanceTest.main(new String[]{});
+//                                // Truyền các luồng dữ liệu
+//                                List<Result> results = NiftygatewayTest.getResults();
+//                                List<CollectionItem>  collectionItems = BinanceTest.getCollectionItem();
+//
+//                                // Truyền dữ liệu vào các panel
+//                                panel_DanhSach.setResults(results);
+//                                panel_DanhSach.setCollectionItem(collectionItems);
+//                                // Gọi các phương thức cần thiết trên các panel khác (nếu cần)
+//                        } catch (IOException | InterruptedException e) {
+//                                e.printStackTrace();
+//                        }
+//                }).start();
+//
+//                SideBar sidebar = new SideBar();
+//                add(sidebar, BorderLayout.WEST);
+//
+//                JPanel content = new JPanel();
+//                ;
+//                add(content, BorderLayout.CENTER);
+//                content.setLayout(new BorderLayout(20, 5));
+//
+//                JPanel panel_Content_Title = new JPanel();
+//                content.add(panel_Content_Title, BorderLayout.NORTH);
+//                panel_Content_Title.setLayout(new FlowLayout(FlowLayout.LEFT, 15, 5));
+//
+//                JLabel lbl_Content_Title = new JLabel("Danh sách NFT"); // ten chuc nang (Danh Sach NFT, Tim Kiem Du Lieu, Thong Ke Du Lieu)
+//                lbl_Content_Title.setFont(new Font("Arial", Font.BOLD, 20));
+//                panel_Content_Title.add(lbl_Content_Title);
+//
+//                JPanel panel_Dem_01 = new JPanel();
+//                content.add(panel_Dem_01, BorderLayout.WEST);
+//
+//                JPanel panel_Dem_02 = new JPanel();
+//                content.add(panel_Dem_02, BorderLayout.EAST);
+//
+//                JPanel panel_3 = new JPanel();
+//                content.add(panel_3, BorderLayout.SOUTH);
+//
+//                JPanel panel_Content_Main = new JPanel();
+//                content.add(panel_Content_Main, BorderLayout.CENTER);
+//                CardLayout cardLayout = new CardLayout(0,0);
+//                panel_Content_Main.setLayout(cardLayout);
+//                panel_Content_Main.add(panel_DanhSach,"1");
+//                panel_Content_Main.add(panel_TimKiem,"2");
+//                panel_Content_Main.add(panel_ThongKe,"3");
+//
+//                Component componentAtIndex = sidebar.getComponent(0);
+//                JButton buttonDanhSach = null, buttonTimKiem = null, buttonThongKe = null;
+//                if (componentAtIndex instanceof JButton) {
+//                        buttonDanhSach = (JButton) componentAtIndex;
+//                }
+//
+//                Component componentAtIndex1 = sidebar.getComponent(1);
+//                if (componentAtIndex instanceof JButton) {
+//                        buttonTimKiem = (JButton) componentAtIndex1;
+//                }
+//
+//                Component componentAtIndex2 = sidebar.getComponent(2);
+//                if (componentAtIndex instanceof JButton) {
+//                        buttonThongKe = (JButton) componentAtIndex2;
+//                }
+//
+//                buttonDanhSach.addActionListener(e -> showCard(panel_Content_Main, "1",lbl_Content_Title,"Danh sách NFT"));
+//                buttonTimKiem.addActionListener(e -> showCard(panel_Content_Main, "2",lbl_Content_Title,"Tìm kiếm dữ liệu"));
+//                buttonThongKe.addActionListener(e -> showCard(panel_Content_Main, "3",lbl_Content_Title,"Thống kê dữ liệu"));
+//        }
+//
+//        private static void showCard(Container container, String cardName, JLabel lbl_Content_Title, String title) {
+//                lbl_Content_Title.setText(title);
+//                CardLayout cardLayout = (CardLayout) container.getLayout();
+//                cardLayout.show(container, cardName);
+//        }
+//}
