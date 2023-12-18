8a32051e02429a2b1451517f92a238759fa7a774
Vumanh-mvp <vuducmanh10a@gmail.com>
2023-12-18 14:09:07 +0700

    Manh fixed the error Crawl

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,11 +1,11 @@
-//package view;
-//
-//import javax.swing.*;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        ManHinhChinh manHinhChinh = new ManHinhChinh();
-//        manHinhChinh.setVisible(true);
-//        manHinhChinh.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//    }
-//}
+package view;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ManHinhChinh manHinhChinh = new ManHinhChinh();
+        manHinhChinh.setVisible(true);
+        manHinhChinh.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
