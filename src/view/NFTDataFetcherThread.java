dd7693567ddc7ae2ce4607efa3656202e445308c
GIGABYTE <nghia.tt213581@sis.hust.edu.vn>
2023-12-19 10:48:44 +0700

    NFTDataFetcherThread - 10:43AM 19/12/2023

--- src/view/NFTDataFetcherThread.java
+++ src/view/NFTDataFetcherThread.java
@@ -1,7 +1,10 @@
 package view;
 
+import OOP.TodayNFTNewsCrawler.TodayNTFCrawler;
 import crawler.BinanceCrawler;
 import crawler.NiftyGateway.NiftyGatewayCrawler;
+import crawler.Opensea.OpenseaCrawler;
+import crawler.Twitter.TwitterCrawler;
 
 public class NFTDataFetcherThread extends Thread{
     @Override
@@ -9,6 +12,9 @@ public class NFTDataFetcherThread extends Thread{
         try {
             NiftyGatewayCrawler.main(new String[]{});
             BinanceCrawler.main(new String[]{});
+            OpenseaCrawler.main(new String[]{});
+            TodayNTFCrawler.main(new String[]{});
+            TwitterCrawler.main(new String[]{});
         } catch (Exception e) {
             // Handle exceptions or log errors if needed
             e.printStackTrace();
