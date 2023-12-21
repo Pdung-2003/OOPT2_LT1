5a8498492b73afa02b09703b792db63cfc98cc99
GIGABYTE <nghia.tt213581@sis.hust.edu.vn>
2023-12-21 16:55:27 +0700

    sửa GeneralConnector - 17:00PM 21/12/2023

--- src/connectors/GeneralConnector.java
+++ src/connectors/GeneralConnector.java
@@ -8,25 +8,30 @@ import services.IJsonHandler;
 import java.util.List;
 
 public class GeneralConnector {
-    public List<Binance> binanceList;
-    public List<NiftyGateway> niftyGatewayList;
-    public List<Opensea> openseaList;
-    public List<TodayNFTNews> todayNFTNewsList;
-    public List<Twitter> twitterList;
-    IJsonHandler gson = new GsonHandler();
-    public GeneralConnector() {
+    private IJsonHandler gson = new GsonHandler();
+    private String binanceDataFile = "Binance.json";
+    private String niftyGatewayDataFile = "NiftyGatewayData.json";
+    private String openseaDataFile = "OpenseaCrawlerRankingPer7Days.json";
+    private String todayNFTNewsFile = "TodayNFTNews.json";
+    private String twitterDataFile = "Twitter.json";
 
+    public List<Binance> getBinanceData() throws Exception {
+        return gson.fromJson(binanceDataFile, new TypeToken<List<Binance>>() {}.getType());
     }
-    public void getData() throws Exception {
-        binanceList = gson.fromJson("Binance.json", new TypeToken<List<Binance>>() {
-        }.getType());
-        niftyGatewayList = gson.fromJson("NiftyGatewayData.json", new TypeToken<List<NiftyGateway>>() {
-        }.getType());
-        openseaList = gson.fromJson("OpenseaCrawlerRankingPer7Days.json", new TypeToken<List<Opensea>>() {
-        }.getType());
-        todayNFTNewsList = gson.fromJson("TodayNFTNews.json", new TypeToken<List<TodayNFTNews>>() {
-        }.getType());
-        twitterList = gson.fromJson("Twitter.json", new TypeToken<List<Twitter>>() {
-        }.getType());
+
+    public List<NiftyGateway> getNiftyGatewayData() throws Exception {
+        return gson.fromJson(niftyGatewayDataFile, new TypeToken<List<NiftyGateway>>() {}.getType());
+    }
+
+    public List<Opensea> getOpenseaData() throws Exception {
+        return gson.fromJson(openseaDataFile, new TypeToken<List<Opensea>>() {}.getType());
+    }
+
+    public List<TodayNFTNews> getTodayNFTNews() throws Exception {
+        return gson.fromJson(todayNFTNewsFile, new TypeToken<List<TodayNFTNews>>() {}.getType());
+    }
+
+    public List<Twitter> getTwitterData() throws Exception {
+        return gson.fromJson(twitterDataFile, new TypeToken<List<Twitter>>() {}.getType());
     }
 }
