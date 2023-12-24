7b1204f98c154e7f9caddc6a4014d94b365f96fc
GIGABYTE <nghia.tt213581@sis.hust.edu.vn>
2023-12-24 18:09:45 +0700

    Thêm thông báo khi cập nhật dữ liệu - 18:09PM

--- src/crawler/RunCrawlerAll.java
+++ src/crawler/RunCrawlerAll.java
@@ -1,26 +1,37 @@
 package crawler;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class RunCrawlerAll {
     public static void main(String[] args) throws IOException, InterruptedException {
-        // Tạo và chạy NiftyGatewayCrawler
-        NiftyGatewayCrawler niftyGatewayCrawler = new NiftyGatewayCrawler();
-        niftyGatewayCrawler.run();
 
-        // Tạo và chạy BinanceCrawler
-        BinanceCrawler binanceCrawler = new BinanceCrawler();
-        binanceCrawler.run();
+        try {
+            JOptionPane.showMessageDialog(null, "Vui lòng chờ đợi dữ liệu được thu thập.");
 
-        // Tạo và chạy TwitterCrawler
-        TwitterCrawler twitterCrawler = new TwitterCrawler();
-        twitterCrawler.run();
+            // Tạo và chạy NiftyGatewayCrawler
+            NiftyGatewayCrawler niftyGatewayCrawler = new NiftyGatewayCrawler();
+            niftyGatewayCrawler.run();
 
-        OpenseaCrawler openseaCrawler   = new OpenseaCrawler();
-        openseaCrawler.run();
+            // Tạo và chạy BinanceCrawler
+            BinanceCrawler binanceCrawler = new BinanceCrawler();
+            binanceCrawler.run();
 
-        TodayNTFCrawler todayNTFCrawler = new TodayNTFCrawler();
-        todayNTFCrawler.run();
-        // Các lệnh khác hoặc xử lý ngoại lệ (nếu cần)
+            // Tạo và chạy TwitterCrawler
+            TwitterCrawler twitterCrawler = new TwitterCrawler();
+            twitterCrawler.run();
+
+            OpenseaCrawler openseaCrawler   = new OpenseaCrawler();
+            openseaCrawler.run();
+
+            TodayNTFCrawler todayNTFCrawler = new TodayNTFCrawler();
+            todayNTFCrawler.run();
+
+            JOptionPane.showMessageDialog(null, "Dữ liệu đã được cập nhật thành công.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Xử lý exception nếu có
+            JOptionPane.showMessageDialog(null, "Có lỗi xảy ra trong quá trình cập nhật dữ liệu.");
+        }
     }
 }
